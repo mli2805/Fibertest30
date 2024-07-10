614378ada66efaf59678700153d88b13a65e1ef1
mli2805 <mli2805@gmail.com>
2024-07-10 07:42:28 +0300

    cleanup

--- WebFt30/src/app/features/dashboard/dashboard.module.ts
+++ WebFt30/src/app/features/dashboard/dashboard.module.ts
@@ -3,9 +3,6 @@ import { NgModule } from '@angular/core';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { TestQueueMonitorComponent } from './components/test-queue-monitor/test-queue-monitor.component';
-import { ActiveAlarmsComponent } from './components/active-alarms/active-alarms.component';
-import { QuickAnalysisComponent } from './components/quick-analysis/quick-analysis.component';
 import { MeasurementModule } from '../shared/measurement/measurement.module';
 import { RtuTreeComponent } from './components/rtu-tree/rtu-tree.component';
 import { OneRtuComponent } from './components/rtu-tree/one-rtu/one-rtu.component';
@@ -19,9 +16,6 @@ import { OneBopComponent } from './components/rtu-tree/one-bop/one-bop.component
   exports: [],
   declarations: [
     DashboardComponent,
-    TestQueueMonitorComponent,
-    ActiveAlarmsComponent,
-    QuickAnalysisComponent,
     RtuTreeComponent,
     OneRtuComponent,
     AttachedTraceComponent,
