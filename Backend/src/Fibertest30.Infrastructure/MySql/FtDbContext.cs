fc04d8acbc5e68b3a8b95eece66707dedb896a68
mli2805 <mli2805@gmail.com>
2024-09-08 09:12:18 +0300

    Login page russification

--- Backend/src/Fibertest30.Infrastructure/MySql/FtDbContext.cs
+++ Backend/src/Fibertest30.Infrastructure/MySql/FtDbContext.cs
@@ -9,7 +9,9 @@ public class FtDbContext : DbContext
     public FtDbContext(DbContextOptions<FtDbContext> options) : base(options) { }
 
 
+#pragma warning disable VSSpell001 // Spell Check
     public DbSet<RtuStation> rtustations { get; set; }
+#pragma warning restore VSSpell001 // Spell Check
     public DbSet<SorFile> sorFiles { get; set; }
     public DbSet<Snapshot> snapshots { get; set; }
 
