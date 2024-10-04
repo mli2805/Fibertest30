e32e8a7caed5f3884f1e2594b24f13fc09e98350
mli2805 <mli2805@gmail.com>
2024-10-04 08:30:32 +0300

    Get StreamIdOfiginal from appsettings.json

--- Backend/src/Fibertest30.Infrastructure/NEventStore/EventStoreInitializer.cs
+++ Backend/src/Fibertest30.Infrastructure/NEventStore/EventStoreInitializer.cs
@@ -1,29 +1,33 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using MySqlConnector;
 using NEventStore;
 using NEventStore.Persistence.Sql.SqlDialects;
 using System.Data.Common;
+using System.Runtime.InteropServices;
 
 namespace Fibertest30.Infrastructure;
 public class EventStoreInitializer
 {
     private readonly ILogger<EventStoreInitializer> _logger;
+    private readonly IConfiguration _configuration;
     private readonly MySerializer _mySerializer;
     private string EventSourcingScheme => "ft20graph";
-    public string EventSourcingConnectionString => 
+    public string EventSourcingConnectionString =>
         $"server=localhost;port=3306;user id=root;password=root;database={EventSourcingScheme}";
-    
+
     // такая же строка ещё используется для конфигурации доступа ко 2й части бд, см ConfigureServices
-    private string FtConnectionString => 
+    private string FtConnectionString =>
         "server=localhost;port=3306;user id=root;password=root;database=ft20efcore";
 
     public Guid StreamIdOriginal { get; set; } = Guid.Empty;
     public IStoreEvents? StoreEvents { get; set; }
 
 
-    public EventStoreInitializer(ILogger<EventStoreInitializer> logger, MySerializer mySerializer)
+    public EventStoreInitializer(ILogger<EventStoreInitializer> logger, IConfiguration configuration, MySerializer mySerializer)
     {
         _logger = logger;
+        _configuration = configuration;
         _mySerializer = mySerializer;
     }
 
@@ -73,16 +77,45 @@ public class EventStoreInitializer
     {
         if (StreamIdOriginal != Guid.Empty) return StreamIdOriginal;
 
+        bool isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+        var commandText = isWindows
+            ? $"SELECT StreamIdOriginal FROM {EventSourcingScheme}.commits LIMIT 1;"
+            : $"SELECT StreamIdOriginal FROM {EventSourcingScheme}.Сommits LIMIT 1;";
+        _logger.LogInformation($"command is {commandText}");
+
+        if (isWindows) return GetByMysqlCommand(commandText);
+
+        // под линуксом дает ошибку нет такой таблицы (не важно с большой или маленькой буквы задаешь)
+        // поэтому временно делаем так - задаем в appsettings.json StreamIdOriginal
+
+        string? ll = _configuration["StreamIdOriginal"];
+        if (ll == null)
+            return Guid.Empty;
+        return Guid.TryParse(ll, out Guid r) ? r : Guid.Empty;
+    }
+
+    private Guid GetByMysqlCommand(string commandText)
+    {
         try
         {
             MySqlConnection connection = new MySqlConnection(EventSourcingConnectionString);
-            MySqlCommand command = new MySqlCommand(
-                $"SELECT StreamIdOriginal FROM {EventSourcingScheme}.Сommits;", connection);
+            _logger.LogInformation($"connection string is {EventSourcingConnectionString}");
+            MySqlCommand command = new MySqlCommand(commandText, connection);
             connection.Open();
-            var result = (string)(command.ExecuteScalar() ?? 0);
+
+            // метод ExecuteReader 
+            var result = "";
+            using var reader = command.ExecuteReader();
+            while (reader.Read()) // limit 1 - 1 строка
+            {
+                result = reader.GetString(0);
+            }
+
+            // var result = (string)(command.ExecuteScalar() ?? 0);
             connection.Close();
             Thread.Sleep(TimeSpan.FromMilliseconds(100));
             StreamIdOriginal = Guid.Parse(result);
+            _logger.LogInformation($"StreamIdOriginal is {StreamIdOriginal}");
             return StreamIdOriginal;
         }
         catch (Exception e)
@@ -90,6 +123,5 @@ public class EventStoreInitializer
             _logger.LogError("GetStreamIdIfExists: " + e.Message);
             return Guid.Empty;
         }
-
     }
 }
---
+++ Backend/src/Fibertest30.Infrastructure/NEventStore/ShellCommand.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace Fibertest30.Infrastructure;
+public static class ShellCommand
+{
+    public static string GetCommandLineOutput(string shCommand)
+    {
+        var cmd = $"-c \"{shCommand}\"";
+        ProcessStartInfo startInfo = new() 
+        { 
+            FileName = "/bin/sh", 
+            Arguments = cmd, 
+            RedirectStandardOutput = true,
+        }; 
+
+        Process proc = new() { StartInfo = startInfo, };
+        proc.Start();
+        string output = proc.StandardOutput.ReadToEnd();
+        proc.WaitForExit();
+
+        return output;
+    }
+}
