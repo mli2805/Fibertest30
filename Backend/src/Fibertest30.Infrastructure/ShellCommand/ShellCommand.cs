e94e93e8c10799aaa49ff797919b475220e6fc7f
mli2805 <mli2805@gmail.com>
2025-04-17 10:44:48 +0300

    little refactoring

--- Backend/src/Fibertest30.Infrastructure/ShellCommand/ShellCommand.cs
+++ Backend/src/Fibertest30.Infrastructure/ShellCommand/ShellCommand.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Extensions.Logging;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 
 namespace Fibertest30.Infrastructure;
 public static class ShellCommand
@@ -7,27 +6,20 @@ public static class ShellCommand
     public static string GetCommandLineOutput(string shCommand)
     {
         var cmd = $"-c \"{shCommand}\"";
-        ProcessStartInfo startInfo = new()
-        {
-            FileName = "/bin/sh",
-            Arguments = cmd,
-            RedirectStandardOutput = true,
-        };
-
-        Process proc = new() { StartInfo = startInfo, };
-        proc.Start();
-        string output = proc.StandardOutput.ReadToEnd();
-        proc.WaitForExit();
-
-        return output;
+        return GetOutput(cmd);
     }
 
     public static string GetScriptOutput(string scriptFilename)
     {
+        return GetOutput(scriptFilename);
+    }
+
+    private static string GetOutput(string startInfoArguments)
+    {
         ProcessStartInfo startInfo = new()
         {
             FileName = "/bin/sh",
-            Arguments = scriptFilename,
+            Arguments = startInfoArguments,
             RedirectStandardOutput = true,
         };
         Process proc = new() { StartInfo = startInfo, };
