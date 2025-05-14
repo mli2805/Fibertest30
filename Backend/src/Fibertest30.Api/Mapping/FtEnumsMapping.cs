b9a231edaf1223016e1b3a922b45d0f2a94916e7
mli2805 <mli2805@gmail.com>
2025-05-14 14:39:49 +0300

    Fix misprinting

--- Backend/src/Fibertest30.Api/Mapping/FtEnumsMapping.cs
+++ Backend/src/Fibertest30.Api/Mapping/FtEnumsMapping.cs
@@ -153,7 +153,7 @@ public static class FtEnumsMapping
             Iit.Fibertest.Dto.OpticalAccidentType.Break => OpticalAccidentType.Break,
             Iit.Fibertest.Dto.OpticalAccidentType.Loss => OpticalAccidentType.Loss,
             Iit.Fibertest.Dto.OpticalAccidentType.Reflectance => OpticalAccidentType.Reflectance,
-            Iit.Fibertest.Dto.OpticalAccidentType.LossCoeff => OpticalAccidentType.TotalLoss,
+            Iit.Fibertest.Dto.OpticalAccidentType.LossCoeff => OpticalAccidentType.LossCoeff,
             Iit.Fibertest.Dto.OpticalAccidentType.TotalLoss => OpticalAccidentType.TotalLoss,
             Iit.Fibertest.Dto.OpticalAccidentType.None => OpticalAccidentType.None,
             _ => throw new ArgumentOutOfRangeException()
