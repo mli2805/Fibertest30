e468e3c3937655caaca15c7df8ae80ef524fe38f
mli2805 <mli2805@gmail.com>
2025-07-12 19:20:04 +0300

    Get rid of Automapper 14 which conflicts with Automapper 10.1.1 from sor libs

--- Backend/src/Fibertest30.Api/Mapping/LandmarksMapping.cs
+++ Backend/src/Fibertest30.Api/Mapping/LandmarksMapping.cs
@@ -1,41 +1,13 @@
-﻿using AutoMapper;
-
-namespace Fibertest30.Api;
-
-public class LandmarksMappingProfile : Profile
-{
-    public LandmarksMappingProfile()
-    {
-        CreateMap<Iit.Fibertest.Graph.ColoredLandmark, ColoredLandmark>()
-            .ForMember(d => d.EquipmentType,
-                opt =>
-                                opt.MapFrom(s => s.EquipmentType.ToProto()))
-            .ForMember(d => d.GpsCoors,
-                opt =>
-                                opt.MapFrom(s => s.GpsCoors.ToProto()));
-
-        CreateMap<ColoredLandmark, Iit.Fibertest.Graph.ColoredLandmark>()
-            .ForMember(d => d.EquipmentType,
-                opt =>
-                    opt.MapFrom(s => s.EquipmentType.FromProto()))
-            .ForMember(d => d.GpsCoors,
-                opt =>
-                    opt.MapFrom(s => s.GpsCoors.FromProto()))
-            .ForMember(d => d.IsChanged,
-                opt => opt.Ignore());
-    }
-}
+﻿namespace Fibertest30.Api;
 
 public static class LandmarksMapping
 {
-    private static readonly IMapper _mapper =
-        new MapperConfiguration(cfg => cfg.AddProfile<LandmarksMappingProfile>()).CreateMapper();
     public static LandmarksModel ToProto(this Application.LandmarksModel model)
     {
         var r = new LandmarksModel()
         {
             LandmarksModelId = model._landmarksModelId.ToString(),
-            Landmarks = { model.Rows.Select(_mapper.Map<ColoredLandmark>) }
+            Landmarks = { model.Rows.Select(ToProto) }
         };
 
         return r;
@@ -43,6 +15,83 @@ public static class LandmarksMapping
 
     public static Iit.Fibertest.Graph.ColoredLandmark FromProto(this ColoredLandmark landmark)
     {
-        return _mapper.Map<Iit.Fibertest.Graph.ColoredLandmark>(landmark);
+        return new Iit.Fibertest.Graph.ColoredLandmark
+        {
+            IsFromBase = landmark.IsFromBase,
+
+            NodeId = Guid.Parse(landmark.NodeId),
+            FiberId = Guid.Parse(landmark.FiberId),
+            Number = landmark.Number,
+            NumberIncludingAdjustmentPoints = landmark.NumberIncludingAdjustmentPoints,
+
+            NodeTitle = landmark.NodeTitle,
+            NodeTitleColor = landmark.NodeTitleColor,
+            NodeComment = landmark.NodeComment,
+            NodeCommentColor = landmark.NodeCommentColor,
+
+            EquipmentId = Guid.Parse(landmark.EquipmentId),
+            EquipmentTitle = landmark.EquipmentTitle,
+            EquipmentTitleColor = landmark.EquipmentTitleColor,
+            EquipmentType = landmark.EquipmentType.FromProto(),
+            EquipmentTypeColor = landmark.EquipmentTypeColor,
+
+            LeftCableReserve = landmark.LeftCableReserve,
+            RightCableReserve = landmark.RightCableReserve,
+            CableReservesColor = landmark.CableReservesColor,
+
+            GpsDistance = landmark.GpsDistance,
+            GpsSection = landmark.GpsSection,
+            IsUserInput = landmark.IsUserInput,
+            GpsSectionColor = landmark.GpsSectionColor,
+
+            OpticalDistance = landmark.OpticalDistance,
+            OpticalSection = landmark.OpticalSection,
+            EventNumber = landmark.EventNumber,
+
+            GpsCoors = landmark.GpsCoors.FromProto(),
+            GpsCoorsColor = landmark.GpsCoorsColor
+        };
+    }
+
+    private static ColoredLandmark ToProto(this Iit.Fibertest.Graph.ColoredLandmark landmark)
+    {
+        return new ColoredLandmark
+        {
+            NodeId = landmark.NodeId.ToString(),
+            FiberId = landmark.FiberId.ToString(),
+        
+            Number = landmark.Number,
+            NumberIncludingAdjustmentPoints = landmark.NumberIncludingAdjustmentPoints,
+        
+            NodeTitle = landmark.NodeTitle,
+            NodeTitleColor = landmark.NodeTitleColor,
+            NodeComment = landmark.NodeComment,
+            NodeCommentColor = landmark.NodeCommentColor,
+        
+            EquipmentId = landmark.EquipmentId.ToString(),
+            EquipmentTitle = landmark.EquipmentTitle,
+            EquipmentTitleColor = landmark.EquipmentTitleColor,
+            EquipmentType = landmark.EquipmentType.ToProto(),
+            EquipmentTypeColor = landmark.EquipmentTypeColor,
+        
+            LeftCableReserve = landmark.LeftCableReserve,
+            RightCableReserve = landmark.RightCableReserve,
+            CableReservesColor = landmark.CableReservesColor,
+        
+            GpsDistance = landmark.GpsDistance,
+            GpsSection = landmark.GpsSection,
+            IsUserInput = landmark.IsUserInput,
+            GpsSectionColor = landmark.GpsSectionColor,
+        
+            OpticalDistance = landmark.OpticalDistance,
+            OpticalSection = landmark.OpticalSection,
+            EventNumber = landmark.EventNumber,
+        
+            GpsCoors = landmark.GpsCoors.ToProto(),
+            GpsCoorsColor = landmark.GpsCoorsColor,
+        
+            IsFromBase = landmark.IsFromBase,
+            IsChanged = landmark.IsChanged,
+        };
     }
 }
